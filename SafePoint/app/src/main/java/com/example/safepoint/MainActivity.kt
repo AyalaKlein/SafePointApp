ac3e0fd4fff54e42bb634cea08433c4b334e3d50
AyalaKlein <aya.k.100@gmail.com>
2020-04-28 17:57:36 +0300

    map with marker from api

--- SafePoint/app/src/main/java/com/example/safepoint/MainActivity.kt
+++ SafePoint/app/src/main/java/com/example/safepoint/MainActivity.kt
@@ -3,7 +3,21 @@ package com.example.safepoint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.common.api.Response
+import io.github.rybalkinsd.kohttp.dsl.httpGet
+import io.github.rybalkinsd.kohttp.ext.asyncHttpGet
+import io.github.rybalkinsd.kohttp.ext.httpGet
+import io.github.rybalkinsd.kohttp.ext.httpGetAsync
+import io.github.rybalkinsd.kohttp.util.Json
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newFixedThreadPoolContext
+import okhttp3.OkHttpClient
+import org.json.JSONArray
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +26,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // TODO: Set listener to Home Front Command API
-        // startActivity(Intent(this, NavigationActivity::class.java))
-        // putExtra(Dest)
-        // finish()
+
+        var json = ""
+//        "http://localhost:5000/api/shelters".httpGet().isSuccessful() =
+
+        var res = ""
+        val scope = CoroutineScope(newFixedThreadPoolContext(1, "synchronizationPool"))
+        scope.launch {
+            res = "http://10.0.2.2:5000/api/shelters".httpGet().body()!!.string()
+//            val shelters = JSONArray(res)
+            val intent = Intent(applicationContext, NavigationActivity::class.java)
+
+            intent.putExtra("shelters", res)
+            startActivity(intent)
+            finish()
+        }
+
+
         profileSettings.setOnClickListener {
             startActivity(Intent(this, ProfileActivity::class.java))
             finish()
--- SafePoint/app/src/main/java/com/example/safepoint/NavigationActivity.kt
+++ SafePoint/app/src/main/java/com/example/safepoint/NavigationActivity.kt
@@ -3,19 +3,41 @@ package com.example.safepoint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_navigation.*
+import org.json.JSONArray
 
-class NavigationActivity : AppCompatActivity() {
+class NavigationActivity : AppCompatActivity(), OnMapReadyCallback {
+    var shelters: JSONArray = JSONArray()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+
+        shelters = JSONArray(intent.getStringExtra("shelters"))
 
         // TODO : set interval on nTimer
 
         nSafe.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        // Add a marker in Sydney and move the camera
+        for (i in 0 until shelters.length()) {
+            val shelter = shelters.getJSONObject(i)!!
+            googleMap.addMarker(MarkerOptions().position(LatLng(shelter["lat"] as Double, shelter["lon"] as Double)))
         }
     }
 }
