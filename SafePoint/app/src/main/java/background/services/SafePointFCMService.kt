fbba692da6cd076748bc5c8da8ffe9318767d2f8
assafzvigoren <assafzvigoren4@gmail.com>
2020-08-18 14:01:55 +0300

    add connection to fcm

--- SafePoint/app/src/main/java/background/services/SafePointFCMService.kt
+++ SafePoint/app/src/main/java/background/services/SafePointFCMService.kt
@@ -1,8 +1,12 @@
 package background.services
 
 import android.util.Log
+import com.example.safepoint.NavigationActivity
+import com.example.safepoint.background.LocationService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import io.github.rybalkinsd.kohttp.dsl.async.httpGetAsync
+import io.github.rybalkinsd.kohttp.ext.url
 
 class SafePointFCMService: FirebaseMessagingService() {
 
@@ -10,6 +14,15 @@ class SafePointFCMService: FirebaseMessagingService() {
         if (remoteMessage.data.isNotEmpty()) {
             Log.d(TAG, "From: ${remoteMessage.from}")
             //TODO update cache
+            LocationService.getLastLocation().addOnCompleteListener {
+                if (it.result != null) {
+                    httpGetAsync {
+                        url("http://10.0.2.2:5000/api/shelters/SearchForShelter?operationGuid=${remoteMessage.data["operationGuid"]}&fcmToken=${"asfd"}&locX=${it.result!!.longitude}&locY=${it.result!!.latitude}")
+                    }.invokeOnCompletion {
+
+                    }
+                }
+            }
         }
     }
 
