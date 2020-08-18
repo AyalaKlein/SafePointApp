41d4631cd667a3130a03e5db8075684c0e9c3adc
assafzvigoren <assafzvigoren4@gmail.com>
2020-08-18 23:34:38 +0300

    add connection to the fcm from the app to server and save the fcm token and the selected shelter on the device cache

--- SafePoint/app/src/main/java/background/services/SafePointFCMService.kt
+++ SafePoint/app/src/main/java/background/services/SafePointFCMService.kt
@@ -1,25 +1,38 @@
 package background.services
 
+import android.content.Context
 import android.util.Log
 import com.example.safepoint.NavigationActivity
+import com.example.safepoint.R
 import com.example.safepoint.background.LocationService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import io.github.rybalkinsd.kohttp.dsl.async.httpGetAsync
+import io.github.rybalkinsd.kohttp.dsl.async.httpPostAsync
+import io.github.rybalkinsd.kohttp.ext.asString
 import io.github.rybalkinsd.kohttp.ext.url
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 class SafePointFCMService: FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         if (remoteMessage.data.isNotEmpty()) {
             Log.d(TAG, "From: ${remoteMessage.from}")
-            //TODO update cache
             LocationService.getLastLocation().addOnCompleteListener {
+                val userSettings = getSharedPreferences(getString(R.string.user_settings), Context.MODE_PRIVATE)
+                val userFcmToken = userSettings.getString(getString(R.string.fcm_token), "")
                 if (it.result != null) {
-                    httpGetAsync {
-                        url("http://10.0.2.2:5000/api/shelters/SearchForShelter?operationGuid=${remoteMessage.data["operationGuid"]}&fcmToken=${"asfd"}&locX=${it.result!!.longitude}&locY=${it.result!!.latitude}")
-                    }.invokeOnCompletion {
+                    GlobalScope.launch {
+                        val response = httpGetAsync {
+                            url("http://10.0.2.2:5000/api/shelters/SearchForShelter?operationGuid=${remoteMessage.data["operationGuid"]}&fcmToken=${userFcmToken}&locX=${it.result!!.longitude}&locY=${it.result!!.latitude}")
+                        }.await()
 
+                        with(userSettings.edit()) {
+                            putString(getString(R.string.selected_shelter), response.asString())
+                            commit()
+                        }
                     }
                 }
             }
@@ -27,7 +40,24 @@ class SafePointFCMService: FirebaseMessagingService() {
     }
 
     override fun onNewToken(newToken: String) {
-        //TODO update token in api
+        val userSettings = getSharedPreferences(getString(R.string.user_settings), Context.MODE_PRIVATE)
+        val oldToken = userSettings.getString(getString(R.string.fcm_token), "")
+
+        GlobalScope.launch {
+            httpPostAsync {
+                url("http://10.0.2.2:5000/api/shelters/ChangeUserFcmToken")
+                body {
+                    form {
+                        "oldToken" to oldToken
+                        "newToken" to newToken
+                    }
+                }
+            }.await()
+            with(userSettings.edit()){
+                putString(getString(R.string.fcm_token), newToken)
+                commit()
+            }
+        }
     }
 
     companion object {
